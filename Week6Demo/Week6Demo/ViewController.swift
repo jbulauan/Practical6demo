e5d51e1e353bb0015c6f4c05311c67a824537c4a
JOHN ISAIAH BULAUAN <jbulauan@10-150-44-70.dhcp-m.deakin.edu.au>
2017-04-20 10:31:50 +1000

    demo6week6

---
+++ Week6Demo/Week6Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week6Demo
+//
+//  Created by JOHN ISAIAH BULAUAN on 20/04/2017.
+//  Copyright © 2017 JOHN ISAIAH BULAUAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
